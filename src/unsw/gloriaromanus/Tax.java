4a726694bd962943470738ed8feebc28f5d3efc4
Jayden Forday <z5165222@ad.unsw.edu.au>
2020-10-28 18:54:37 +1000

    Tax class is completed for now

--- src/unsw/gloriaromanus/Tax.java
+++ src/unsw/gloriaromanus/Tax.java
@@ -12,4 +12,34 @@ public class Tax {
         this.taxRate = 0.10;
         this.growth = 10;
     }
+
+    public void changeTax(String taxName){
+        if( taxName.equals("Low") ){
+            this.taxRate = 0.10;
+            this.growth = 10;
+        }
+        else if ( taxName.equals("Normal") ){
+            this.taxRate = 0.15;
+            this.growth = 0;
+        }
+        else if ( taxName.equals("High") ){
+            this.taxRate = 0.20;
+            this.growth = -10;
+        }
+        else if ( taxName.equals("Very high") ){
+            this.taxRate = 0.25;
+            this.growth = -30;
+        }
+    }
+
+    public double getTaxRate(){
+        return this.taxRate;
+    }
+    public int getTaxGrowth(){
+        return this.growth;
+    }
+    public String getTaxType(){
+        return this.taxType;
+    }
+
 }
