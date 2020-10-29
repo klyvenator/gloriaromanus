6cb890481c6793d829656705ffa926132aa64512
Jayden Forday <z5165222@ad.unsw.edu.au>
2020-10-29 18:31:50 +1000

    changed the Town class to implement a TownTest file and fixed taxType bug in Tax.java

--- src/unsw/gloriaromanus/Town.java
+++ src/unsw/gloriaromanus/Town.java
@@ -1,37 +1,63 @@
 package unsw.gloriaromanus;
 
-import unsw.gloriaromanus.Tax;
 
 public class Town {
     
-    private Faction faction;
+    // TO DO uncomment when faction has been created 
+    //private Faction faction; 
+    private String faction;
     private int wealth;
     private Tax tax;
 
-    public Town(Faction faction){
-        this.faction = faction;
+    // TO DO uncomment when faction has been created 
+    // public Town(Faction faction){
+    public Town(){
+        this.faction = "Temp Faction";
         this.tax = new Tax(); // set tax rate to low
         this.wealth = 10; // set initial province wealth to 10
     }
     public int getWealth(){
         return this.wealth;
     }
-    
-    public int getTaxOwed(){
-        if(this.wealth <= 0 ){
-            return 0;
-        }
-        int taxOwed = (int)Math.round( this.tax.getTaxRate() * this.wealth);
-        return taxOwed;
+    public String getFaction(){
+        return this.faction;
     }
+    public int getTaxOwed(){
+        int currWealth = getWealth();
+        if( currWealth <= 0 ) return 0;
 
-    public void wealthGrowth(){
+        int taxOwed = (int)Math.round( this.tax.getTaxRate() * currWealth);
+        if( taxOwed < 1 ) return 0; 
+        else return taxOwed;
+    }
+    private void growWealth(){
         int growth = tax.getTaxGrowth();
-        if( this.wealth + growth <= 0 ){
+        int total = getWealth() + growth;
+        if( total <= 0 ){
             this.wealth = 0;
         }else {
-            this.wealth = this.wealth + growth;
+            this.wealth = total;
         }
+    }
+    public void wealthAfterTax(){
+        growWealth();
+        int currWealth = getWealth();
+        if( currWealth <= 0 ){
+            return;
+        }else{ 
+            int total = currWealth - getTaxOwed();
+            if( total <= 0 ){ 
+                this.wealth = 0;
+            }else{
+                this.wealth = total;
+            }
+        }
+    }
+    public String getTaxStatus(){
+        return tax.getTaxType();
+    }
+    public void updateTaxStatus(String taxType){
+        tax.changeTax(taxType);
     }
     
 }
