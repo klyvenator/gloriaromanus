af32dab777c46a7eff281b0726ca787b42dd7c7e
Jibitesh Saha <z5280740@student.unsw.edu.au>
2020-10-31 11:10:26 +1100

    Implemented decision making of choosing melee vs ranged engagement for when 1 unit is melee, other is ranged

--- src/unsw/gloriaromanus/Skirmishes.java
+++ src/unsw/gloriaromanus/Skirmishes.java
@@ -2,6 +2,7 @@ package unsw.gloriaromanus;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Skirmishes {
     private Unit unitA;
@@ -25,11 +26,41 @@ public class Skirmishes {
 
     private void decideEngagementType() {
         if (unitA == "melee" && unitB == "melee") {
-            engagement = new MeleeEngagement();
+            engagement = new MeleeEngagements();
         } else if (unitA == "ranged" && unitB == "ranged") {
-            engagement = new RangedEngagement();
+            engagement = new RangedEngagements();
         } else {
+            // Set up probabilities of melee / ranged engagement
+            double baseMelee = 0.5; // base-level 50% chance for a melee engagement
 
+            // Adjust according to units' speed values
+            double changeMelee;
+            if (unitA == "melee") {
+                // A is melee
+                changeMelee = unitA.getSpeed() - unitB.getSpeed();
+            } else {
+                // B is melee
+                changeMelee = unitB.getSpeed() - unitA.getSpeed();
+            }
+            changeMelee *= 0.1;
+
+            double chanceMelee = baseMelee + changeMelee;
+
+            // Set the boundaries for the chance of a Melee engagement
+            if (chanceMelee < 0.05) {
+                chanceMelee = 0.05;
+            } else if (chanceMelee > 0.95) {
+                chanceMelee = 0.95;
+            }
+
+            // Toss the dice and decide
+            Random r = new Random();
+            double diceRoll = r.nextDouble();
+            if (diceRoll < chanceMelee) {
+                engagement = new MeleeEngagements();
+            } else {
+                engagement = new RangedEngagements();
+            }
         }
     }
     public Unit startEngagements() {
