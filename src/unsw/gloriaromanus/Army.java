2fd4336e1d569f750b0ff202f986149b9564a9f6
Jibitesh Saha <z5280740@student.unsw.edu.au>
2020-10-31 11:33:41 +1100

    Done random unit selection from given army

--- src/unsw/gloriaromanus/Army.java
+++ src/unsw/gloriaromanus/Army.java
@@ -2,6 +2,7 @@ package unsw.gloriaromanus;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Army {
     private List<Unit> units;
@@ -17,7 +18,21 @@ public class Army {
     public int getNumUnits() {
         return units.size();
     }
-    Unit randomlySelectUnit() {
-        return null;
+
+    /**
+     * Returns a random unit from this army
+     * (uniformly random)
+     * @return unit, randomly (uniformly) chosen
+     */
+    public Unit randomlySelectUnit() {
+        
+        // If this army has no units
+        if (units.size() == 0) {
+            return null;
+        }
+
+        Random r = new Random();
+        
+        return units.get(r.nextInt(units.size()));
     }
 }
