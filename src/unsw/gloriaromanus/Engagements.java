00b4c2ad48f4afb2b0d72069fd2e716ff303d504
Jibitesh Saha <z5280740@student.unsw.edu.au>
2020-10-31 21:36:05 +1100

    Misc. minor changes

--- src/unsw/gloriaromanus/Engagements.java
+++ src/unsw/gloriaromanus/Engagements.java
@@ -3,9 +3,9 @@ package unsw.gloriaromanus;
 public interface Engagements {
     public Unit engage(Unit a, Unit b);
     
-    public Unit route(Unit routing, Unit pursuing);
+    public Unit routeEngage(Unit routing, Unit pursuing);
     
     // TODO Should this attack method be a member of the Unit class?
     // To make applying / removing buffs / debuffs easier?
-    public Unit attack(Unit a, Unit b);
+    public void attack(Unit attacker, Unit defender);
 }
