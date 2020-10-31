484f1d63616f8f71cfcf611004ce970bcf445fce
Jibitesh Saha <z5280740@student.unsw.edu.au>
2020-10-31 15:49:05 +1100

    Began routing framework

--- src/unsw/gloriaromanus/Engagements.java
+++ src/unsw/gloriaromanus/Engagements.java
@@ -1,6 +1,11 @@
 package unsw.gloriaromanus;
 
 public interface Engagements {
-    public Unit startEngagement(Unit a, Unit b);
+    public Unit engage(Unit a, Unit b);
+    
+    public Unit route(Unit a, Unit b);
+    
+    // TODO Should this attack method be a member of the Unit class?
+    // To make applying / removing buffs / debuffs easier?
     public Unit attack(Unit a, Unit b);
 }
