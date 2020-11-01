aaaa8a939dd12ea5f8eadc5687b93806be4078d9
Jayden Forday <z5165222@ad.unsw.edu.au>
2020-11-01 16:59:42 +1000

    almost finished, needs to add one more object and write to file properly

--- src/test/SaveFileTest.java
+++ src/test/SaveFileTest.java
@@ -24,16 +24,35 @@ public class SaveFileTest {
     public void createSaveFile() throws IOException {
         // just one faction for now
         List<Faction> facList = new ArrayList<Faction>();
+        List<Town> townList = new ArrayList<Town>();
+        // create each faction from the givenlist and add it to the Faction list
         for(String s : givenList){
             Faction newFac = new Faction(s);
             facList.add(newFac);
         }
+        // create each town from smallTowns and add to the Town list
         Faction f1 = facList.get(0);
         for(String st : smallTowns){
             f1.addTown(f1, st);
         }
-        saveFile = new SaveFile(facList, "testSave");
+        townList = f1.getTowns();
+        // create some units and add them to Sydney and Melbourne
+        createSomeUnits("Melee Infantry", f1, townList.get(0));
+        createSomeUnits("Elephant", f1, townList.get(1));
         File temp = new File("testSave.json"); 
         temp.delete();
+        saveFile = new SaveFile(facList, "testSave");
+        System.out.println(saveFile);
+        temp = new File("testSave.json"); 
+        temp.delete();
+    }
+
+    public void createSomeUnits(String name, Faction faction, Town town){
+        UnitFactory factory = new UnitFactory();
+        factory.initialise();
+        Unit unit = factory.createUnit(name, faction, town);
+        town.addUnit(unit);
+        //List<Unit> u = town.getUnits();
+       // System.out.println(u.get(0).getName());
     }
 }
