4ea390692f13603be841e4b2088a3c4b1bb182a2
klyvenator <klyvenator@gmail.com>
2020-11-14 00:06:36 +1100

    added a fix which made the provinces clickable again

--- src/unsw/gloriaromanus/Controller/GloriaRomanusController.java
+++ src/unsw/gloriaromanus/Controller/GloriaRomanusController.java
@@ -277,7 +277,7 @@ public class GloriaRomanusController{
                 Feature f = features.get(0);
                 String province = (String)f.getAttributes().get("name");
 
-                if (provinceToOwningFactionMap.get(province).equals(humanFaction)){
+                if (getFaction(province).getFactionName().equals(humanFaction)){
                   // province owned by human
                   if (currentlySelectedHumanProvince != null){
                     featureLayer.unselectFeature(currentlySelectedHumanProvince);
@@ -394,6 +394,15 @@ public class GloriaRomanusController{
       }
     }
     return facList;
+  }
+
+  public Faction getFaction(String provinceName) {
+    for (Town t: provinceToOwningFactionMap.keySet()){
+      if (t.getTownName().equals(provinceName)) {
+        return provinceToOwningFactionMap.get(t);
+      }
+    }
+    return null;
   }
 
   public void setFactionList(List<String> listOfFactionNames){
