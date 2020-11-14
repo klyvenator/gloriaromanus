cd91814cbb6bf4166dc51d78efe10dcd7d63f3cc
Jibitesh Saha <z5280740@student.unsw.edu.au>
2020-11-14 15:21:06 +1100

    Updated battleresolver and skrimishes to write to TextArea

--- src/unsw/gloriaromanus/Model/BattleResolver.java
+++ src/unsw/gloriaromanus/Model/BattleResolver.java
@@ -1,18 +1,18 @@
 package unsw.gloriaromanus.Model;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import javafx.scene.control.TextArea;
 import unsw.gloriaromanus.Model.Enums.BattleStatus;
 import unsw.gloriaromanus.Model.Enums.FightStatus;
 
 public class BattleResolver {
-    private Army armyA;
-    private Army armyB;
+    private Army armyA; // attacker
+    private Army armyB; // defender
     private Town invaderProvince;
     private Town defenderProvince;
     private BattleStatus status;
+    private TextArea terminal;
 
     private BattleResolver() {
         armyA = null;
@@ -20,12 +20,18 @@ public class BattleResolver {
         invaderProvince = null;
         defenderProvince = null;
         status = BattleStatus.FIGHTING;
+        terminal = null;
     }
 
     public BattleResolver(Army a, Army b) {
         this();
         this.armyA = a;
         this.armyB = b;
+    }
+
+    public BattleResolver(Army a, Army b, TextArea terminal) {
+        this(a, b);
+        this.terminal = terminal;
     }
 
     public BattleResolver(Army a, Army b, Town defenderTown) {
@@ -42,42 +48,48 @@ public class BattleResolver {
         return status;
     }
 
+    private void writeToTerminal(String msg) {
+        if (terminal != null) {
+            terminal.appendText(msg + "\n");
+        }
+    }
+
     private void printStartSkirmishMessage(Skirmishes fight) {
         // TODO Print "skirmish started with A and B!"
-        System.out.println("Skirmish START!");
+        writeToTerminal("Skirmish START!");
     }
     private void printEndSkirmishMessage(Skirmishes fight) {
         FightStatus status = fight.getStatus();
         switch (status) {
             case WIN_A:
-                System.out.println("Unit A wins!");
+                writeToTerminal("Unit A wins!");
                 break;
             
             case WIN_B:
-                System.out.println("Unit B wins!");
+                writeToTerminal("Unit B wins!");
                 break;
 
             case FLEE_A:
-                System.out.println("Unit A successfully routed!");
+                writeToTerminal("Unit A successfully routed!");
                 break;
 
             case FLEE_B:
-                System.out.println("Unit B successfully routed!");
+                writeToTerminal("Unit B successfully routed!");
                 break;
 
             case FLEE_ALL:
-                System.out.println("Both units successfully routed!");
+                writeToTerminal("Both units successfully routed!");
                 break;
 
             case DRAW:
-                System.out.println("It's a draw!");
+                writeToTerminal("It's a draw!");
                 break;
 
             case FIGHTING: default:
                 // This should never happen!
             break;
         }
-        System.out.println("-------------------------");
+        writeToTerminal("-------------------------");
     }
 
     /**
@@ -101,7 +113,7 @@ public class BattleResolver {
             selectedA = armyA.randomlySelectAvailableUnit();
             selectedB = armyB.randomlySelectAvailableUnit();
             
-            Skirmishes skirmish = new Skirmishes(selectedA, selectedB);
+            Skirmishes skirmish = new Skirmishes(selectedA, selectedB, terminal);
             
             printStartSkirmishMessage(skirmish);
             
