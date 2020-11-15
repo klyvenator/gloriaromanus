0dbcfb41534db80d98d9dcf05d875b28e8887eb2
Jibitesh Saha <z5280740@student.unsw.edu.au>
2020-11-16 10:27:49 +1100

    Updated town wealth display to connect to backend

--- src/unsw/gloriaromanus/Controller/GloriaRomanusController.java
+++ src/unsw/gloriaromanus/Controller/GloriaRomanusController.java
@@ -357,7 +357,7 @@ public class GloriaRomanusController{
             provinceTown = town;
             Faction factionObject = provinceToOwningFactionMap.get(provinceTown);
             String faction = factionObject.getFactionName();
-            TextSymbol t = new TextSymbol(10,
+            TextSymbol t = new TextSymbol(provinceTown.getWealth(),
             faction + "\n" + provinceTown.getTownName() + "\n", 0xFFFF0000,
             HorizontalAlignment.CENTER, VerticalAlignment.BOTTOM);
 
@@ -618,6 +618,7 @@ public class GloriaRomanusController{
 
       Town randTown = randomFaction.addTown(randomFaction, randomTown);
       randTown.setArmy(new Army(randTown));
+      randTown.setWealth(300);
 
       list.remove(randomIndex);
     }
